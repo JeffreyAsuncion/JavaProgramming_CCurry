da89893f2d17f43dc84f840f408efb14d2d96769
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-24 16:17:42 -0400

    NumericTypes

---
+++ NumericTypes.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class NumericTypes {
+
+    public static void main(String[] args) {
+    
+         System.out.println("Integer");
+	 System.out.println(Integer.MIN_VALUE);
+         System.out.println(Integer.MAX_VALUE);
+         System.out.println(Integer.SIZE);
+
+	 
+         System.out.println("Double");
+
+	 System.out.println(Double.MIN_VALUE);
+	 System.out.println(Double.MAX_VALUE);
+	 System.out.println(Double.SIZE);
+
+    }
+}
