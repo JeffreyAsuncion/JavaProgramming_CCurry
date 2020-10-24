114f79f4a3f717719c9a35192f10fa406e9398c2
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-23 22:14:35 -0400

    README and MySweetProgram

---
+++ MySweetProgram.java
@@ -0,0 +1,13 @@
+
+public class MySweetProgram {
+
+    public static void main(String[] args) { // methods
+        System.out.println("Hello there!");
+    
+    }
+
+    String x = new String(); //property 
+
+}
+
+
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JavaProgramming_CCurry
+
+Java Programming All-in-One Tutorial 6 hour course with Caleb Curry
+
+2020/10/23 - Start tutorial
