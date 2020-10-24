15ef95fe9221d682011111414175c170e673ff05
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-24 15:30:23 -0400

    PrimitiveVariables2

---
+++ PrimitiveVariables2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+
+public class PrimitiveVariables2 {
+
+
+    public static final double PI = 3.14159254;
+
+
+
+    public static void main(String[] args) { // methods
+        
+	double  x = 5; // 2 step process
+	
+	int y; // 1. declaration
+	y = 2; // 2. initialization
+
+	final int Y_IS_CONSTANT = 5;  // Constant variable local to main()
+
+	x  = PrimitiveVariables2.PI * 2;
+
+
+	boolean IsPizzaDelicious = true;
+	byte b = 127;
+	char c = 'Z';
+	short s = -32768;
+	int j = 4343434;
+	long l = 50505050505050L;
+
+	float f = 20.5f; // 32 byte
+	double d = 20.5; //64 byte
+
+
+
+    }
+
+}
+
+// boolean, byte, char, short, int, long, float and double
+
+// variable creation
+// 1. declare
+// 2. initialize
