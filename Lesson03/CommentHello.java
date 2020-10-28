164f84945164b8fdf28f99a6331fc3c8b660784a
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-28 10:39:17 -0400

    Understanding Java Foundation

---
+++ Lesson03/CommentHello.java
@@ -0,0 +1,25 @@
+public class CommentHello {
+    // public or private are access modifiers
+
+    String x = new String("hello"); //property
+
+    // static in OOP means does not have to be instantiated	
+    public static void main(String[] args) { //method
+    
+        System.out.println("Hello World!");
+    } // curly braces wrap code blocks
+
+    // white space is mostly for readability
+    
+
+}
+
+// comment
+// class = Contains everything - has members
+// methods - do something
+// statements - Tell the computer something
+// properties - store something
+//
+// access modifier (public) - who can use?
+// static - No instance of class is needed
+// object - instance of a class
