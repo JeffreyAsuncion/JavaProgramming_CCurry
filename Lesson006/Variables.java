56ffebd4fa4019bef0bcd97ba41961afc7fb246a
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-28 11:16:15 -0400

    Variables

---
+++ Lesson006/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+
+    public static void main(String[] args) {
+        // long way
+        String string = new String("This is a string");
+        // shorthand
+	String easier = "Wow, that was easy!" + string;  // expression a combination of variables
+
+        System.out.println(easier + " :)");	
+    }
+}
+
+
+// operator = work on operands to produce a value
+// operand - the things the operator works on
