63241f54747dfb09e9a62075efb43b0e6bb1725e
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-28 10:55:30 -0400

    Arguments and Parameters

---
+++ Lesson004/ArgsAndParams.java
@@ -0,0 +1,29 @@
+// to run type :
+// java ArgsAndParams <your_name>>
+
+public class ArgsAndParams {
+	    
+    // using args   
+    public static void main(String[] args) {
+	    
+	System.out.println("Hello " + args[0]);
+	
+	} 
+	    
+	    
+}
+	    	                                
+// comment	                                 
+//
+// class = Contains everything - has members	                                 
+// methods - do something
+// arguments - What you pass to a method (part of the calling)
+// parameter - Variables to store arguments (part of definition)
+//
+// statements - Tell the computer something                
+// properties - store something
+
+	                                 
+// access modifier (public) - who can use?          
+// static - No instance of class is needed
+// object - instance of a class
