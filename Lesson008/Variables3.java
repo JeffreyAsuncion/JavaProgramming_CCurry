70a7b2546ecd67e1aeacda7762d06b7ee3eea6ea
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-28 11:50:12 -0400

    Declaration and Initialization

---
+++ Lesson008/Variables3.java
@@ -0,0 +1,36 @@
+//This class is broken just for definitions 
+
+
+public class Variables3 {
+
+    // Global Variable constant
+    public static final double PI = 3.1415;
+
+    public static void main(String[] args) {
+    
+        int y = 4; // 2 step process
+	    
+	int x; // declare
+	x = 5; // initialize
+
+
+	final int Y = 500; // define a constant
+
+	Y = 4; // will produce an error
+
+        // CONSTANTS
+	//   PI
+	//   Variables3.PI outside the program
+	
+	// Block and local variables
+	if () {
+	    int q = 5; // q is only defined in this block of { } 
+	}
+
+	q = 10; // q is not defined here
+    }
+}
+
+// variable creation
+// 1. declare
+// 2. initialize
