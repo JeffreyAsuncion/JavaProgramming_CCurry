0112459a3862f114a5b0503ef1bd8ccfc0a732ea
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-28 12:00:02 -0400

    Primative Types

---
+++ Lesson009/PrimativeTypes.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class PrimativeTypes {
+
+    public static void main(String[] args) {
+        
+	boolean isPizzaDelicious = True;
+	byte b = 'C';
+	char c = 'Z';
+	short s = -32768;
+        int i = 561;
+	long l = 45464564564564L; // note the L
+	double y = 20.5; // double 64 bit but float is 32 bit
+	float f = 20.5f; // note the f
+
+    }
+}
+
+// you get error when you store larger numbers in a smaller datatype
+//
+// boolean, byte, char, short, int, long, double, float
+//
+// Typecasting is available but there will be loss of data
