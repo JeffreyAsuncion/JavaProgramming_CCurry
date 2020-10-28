f5c1cddb5f332f7577c5cfae502a820a021c7ece
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-28 14:29:09 -0400

    Numeric Data Types and Properties

---
+++ Lesson011/NumericDataType.java
@@ -0,0 +1,14 @@
+public class NumericDataType {
+
+    public static void main(String[] args) {
+    
+        int x = 5;
+	Integer y = 5;
+	
+	System.out.println(Integer.MIN_VALUE);
+	System.out.println(Double.MAX_VALUE);
+	System.out.println(Double.POSITIVE_INFINITY);
+	System.out.println(Double.NaN);
+	System.out.println(Double.isNaN(Math.sqrt(-1)));
+    }
+}
